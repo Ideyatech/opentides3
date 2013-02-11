27ce68b999d3e3df69ceedde3b69dea830a5e2b0
Allan Tan <allan@ideyatech.com>
2013-02-11 17:22:40 +0800

    added clan example

---
+++ opentides3-core/src/test/java/com/ideyatech/example/Clan.java
@@ -0,0 +1,97 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.    
+ */
+package com.ideyatech.example;
+
+import java.util.Set;
+
+import org.opentides.bean.BaseEntity;
+
+/**
+ * @author allantan
+ *
+ */
+public class Clan extends BaseEntity {
+
+	private static final long serialVersionUID = -5332804497128647838L;
+
+	private String name;
+	
+	private String description;
+
+	private Ninjago leader;
+	
+	private Set<Ninjago> members;
+
+	/**
+	 * @return the name
+	 */
+	public final String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public final void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public final String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public final void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the leader
+	 */
+	public final Ninjago getLeader() {
+		return leader;
+	}
+
+	/**
+	 * @param leader the leader to set
+	 */
+	public final void setLeader(Ninjago leader) {
+		this.leader = leader;
+	}
+
+	/**
+	 * @return the members
+	 */
+	public final Set<Ninjago> getMembers() {
+		return members;
+	}
+
+	/**
+	 * @param members the members to set
+	 */
+	public final void setMembers(Set<Ninjago> members) {
+		this.members = members;
+	}
+	
+}
