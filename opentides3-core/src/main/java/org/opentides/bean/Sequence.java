ea0bd8c530840fb0e940c8498ff59a8b8b6d8b93
gino <gino@ideyatech.com>
2013-09-25 10:58:27 +0800

    Implemented hash and equals method

--- opentides3-core/src/main/java/org/opentides/bean/Sequence.java
+++ opentides3-core/src/main/java/org/opentides/bean/Sequence.java
@@ -78,4 +78,29 @@ public class Sequence extends BaseEntity {
 		}
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sequence other = (Sequence) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+	
 }
