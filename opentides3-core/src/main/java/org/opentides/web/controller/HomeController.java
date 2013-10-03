21ae2d7d338d56518fafd2fe3b0de4e594d4e022
gino <gino@ideyatech.com>
2013-10-03 15:53:16 +0800

    Removed security for tutorial app and modified HomeController to separate login from home page

--- opentides3-core/src/main/java/org/opentides/web/controller/HomeController.java
+++ opentides3-core/src/main/java/org/opentides/web/controller/HomeController.java
@@ -1,26 +1,20 @@
 package org.opentides.web.controller;
 
-import org.opentides.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@Autowired
-	private UserService userService;
-	
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String index(ModelMap modelMap) {
-		
-		if(userService.getCurrentUser() != null){
-			return "home";
-		} else 
-			return "login";
-		
+	public String index() {
+		return "home";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "login";
 	}
 	
 }
