451311ad0d2fc5902df30ec5725e0b85089cd530
gino <gino@ideyatech.com>
2013-10-09 16:15:36 +0800

    BaseCrudService test

--- opentides3-core/src/test/java/com/ideyatech/service/impl/NinjaServiceImpl.java
+++ opentides3-core/src/test/java/com/ideyatech/service/impl/NinjaServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ideyatech.service.impl;
 
 import org.opentides.service.impl.BaseCrudServiceImpl;
+import org.springframework.stereotype.Service;
 
 import com.ideyatech.bean.Ninja;
 import com.ideyatech.service.NinjaService;
 
+@Service(value = "ninjaService")
 public class NinjaServiceImpl extends BaseCrudServiceImpl<Ninja> implements
 		NinjaService {
 
