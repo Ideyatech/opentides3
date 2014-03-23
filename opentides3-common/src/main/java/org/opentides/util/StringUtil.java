caf8d1f63baf3008b440febcdb6a774da78bab79
allanctan <allan@ideyatech.com>
2014-03-23 08:58:32 +0800

    upgrading to 3.1.0

--- opentides3-common/src/main/java/org/opentides/util/StringUtil.java
+++ opentides3-common/src/main/java/org/opentides/util/StringUtil.java
@@ -269,7 +269,10 @@ public class StringUtil {
     	char[] values = {'a','b','c','d','e','f','g','h','i','j',
     					 'k','l','m','n','o','p','q','r','s','t',
     					 'u','v','w','x','y','z','0','1','2','3',
-    					 '4','5','6','7','8','9'};
+    					 '4','5','6','7','8','9','A','B','C','D',
+    					 'E','F','G','H','I','J','K','L','M','N',
+    					 'O','P','Q','R','S','T','U','V','W','X',
+    					 'Y','Z'};
     	String out = "";
     	for (int i=0;i<length;i++) {
         	int idx=random.nextInt(values.length);
@@ -303,7 +306,7 @@ public class StringUtil {
      * @return
      * @throws Exception
      */
-	@SuppressWarnings("restriction")
+	@SuppressWarnings("restriction")	
 	public static String encrypt(String userId) {	
 		sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
 
