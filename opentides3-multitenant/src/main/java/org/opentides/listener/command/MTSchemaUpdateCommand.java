4a779bf9e7e1e33daa9479d56680b992e5711132
Allan Tan <allan@ideyatech.com>
2015-01-16 13:00:41 +0800

    rename command

--- opentides3-multitenant/src/main/java/org/opentides/listener/command/MTSchemaUpdateCommand.java
+++ opentides3-multitenant/src/main/java/org/opentides/listener/command/MTSchemaUpdateCommand.java
@@ -31,7 +31,7 @@ import org.springframework.stereotype.Component;
  * @author allanctan
  */
 @Component
-public class MTSchemaUpdateCommand implements Command {
+public class MTSchemaUpdateCommand implements StartupCommand {
 
 	private static final Logger _log = Logger
 			.getLogger(MTSchemaUpdateCommand.class);
