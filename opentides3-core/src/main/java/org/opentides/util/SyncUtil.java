0fc37a3999c79c7ec3b6e896bc3d98e799eac95c
Allan Tan <allan@ideyatech.com>
2015-11-12 16:57:27 +0800

    fixes to multi-tenant

---
+++ opentides3-core/src/main/java/org/opentides/util/SyncUtil.java
@@ -0,0 +1,233 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.    
+*/
+
+package org.opentides.util;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.opentides.bean.BaseEntity;
+
+/**
+ * 
+ * @author allantan
+ *
+ */
+public class SyncUtil {
+
+	/**
+	 * Overloaded method to build insert statement, including parent fields.
+	 * @param obj
+	 * @return
+	 */
+	public static String[] buildInsertStatement(BaseEntity obj) {
+		return buildInsertStatement(obj, null, true);
+	}
+
+	/**
+	 * Builds the insert statement for sqlLite.
+	 * @return
+	 */
+	public static String[] buildInsertStatement(BaseEntity obj, String tableName, boolean includeParent) {
+		StringBuilder sql = new StringBuilder("insert into ");
+		StringBuilder columns = new StringBuilder("(");
+		StringBuilder values = new StringBuilder("(");
+
+		String[] insertQuery = new String[2];
+		StringBuilder param = new StringBuilder("[");
+
+		List<String> fields = CacheUtil.getPersistentFields(obj);
+		Map<String, String> columnFields = CacheUtil.getColumnNames(obj,includeParent);
+		int count = 0;
+
+		for (String field : fields) {
+			Object ret = CrudUtil.retrieveNullableObjectValue(obj, field);
+			String column = columnFields.get(field);
+			if (column != null) {
+				String[] cols = column.split(",");
+				if (cols != null && cols.length > 0) {
+					for (int i = 0; i < cols.length; i++) {
+						String n = normalizeValue(ret);
+						if (n.trim().length() > 0) {
+							if (count++ > 0) {
+								columns.append(",");
+								values.append(",");
+								param.append(",");
+							}
+							columns.append(cols[i]);
+							values.append("?");
+							param.append(n);
+
+						}
+					}
+				}				
+			}
+		}
+
+		columns.append(")");
+		values.append(")");
+		
+		if (StringUtil.isEmpty(tableName))
+			tableName = NamingUtil.toSQLName(obj.getClass().getSimpleName());
+
+		sql.append(tableName).append(" ").append(columns).append(" values ")
+				.append(values);
+		insertQuery[0] = sql.toString();
+
+		param.append("]");
+		insertQuery[1] = param.toString();
+
+		return insertQuery;
+	}
+	
+	/**
+	 * Overloaded method to build update statement, including parent fields.
+	 * @param obj
+	 * @return
+	 */	
+	public static String buildUpdateStatement(BaseEntity obj, List<String> fields) {
+		return SyncUtil.buildUpdateStatement(obj, fields, null, true);
+	}
+	
+	/**
+	 * Builds the insert statement for sqlLite.
+	 * @return
+	 */
+	public static String buildUpdateStatement(BaseEntity obj, List<String> fields, String tableName, boolean includeParent) {
+		if (tableName == null)
+			tableName = NamingUtil.toSQLName(obj.getClass().getSimpleName());
+		
+		Map<String, String> columns = CacheUtil.getColumnNames(obj, includeParent);
+		
+		StringBuilder sql = new StringBuilder("update ");
+		sql.append(tableName)
+		   .append(" set ");		
+		
+		int count = 0;
+		for (String field:fields) {
+			String columnName = columns.get(field);
+			Object ret = CrudUtil.retrieveNullableObjectValue(obj, field);
+			String n = normalizeValue(ret);
+			if (n.trim().length() > 0) {
+				if (count++ > 0) {
+					sql.append(",");
+				}
+				sql.append(columnName)
+				   .append("=")
+				   .append(n);
+			}
+		}
+		sql.append(" where id=")
+		   .append(obj.getId());
+		return sql.toString();
+	}
+	
+	/**
+	 * Builds the delete statement for sqlLite.
+	 * @return
+	 */
+	public static String buildDeleteStatement(BaseEntity obj) {
+		String tableName = NamingUtil.toSQLName(obj.getClass().getSimpleName());
+		StringBuilder sql = new StringBuilder("delete from ");
+		sql.append(tableName).append(" where id=").append(obj.getId());
+		return sql.toString();
+	}
+	
+	/**
+	 * Override this method for additional transformation of values.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public static String doNormalizeValue(Object obj) {
+		return "";
+	}
+	
+    /**
+     * Private helper that converts object into other form for 
+     * audit log comparison and display purposes.
+     * @param obj
+     * @return
+     */
+	private static String normalizeValue(Object obj) {
+		if (obj == null)
+			return "";
+
+		if (obj instanceof Collection) {
+			// ignore, we are not returning collections.
+			return "";
+		}
+
+		String value = SyncUtil.doNormalizeValue(obj);
+		if (!StringUtil.isEmpty(value)) {
+			return value;
+		}
+		
+//		if (obj instanceof Money) {
+//			Money money = (Money) obj;
+//
+//			if (column.toLowerCase().contains("currency")) {
+//				return "'" + money.getCurrencyUnit().getCurrencyCode() + "'";
+//			} else {
+//				return "" + money.getAmount();
+//			}
+//		}
+
+		// no quotes needed
+		if (obj instanceof Long || obj instanceof Integer
+				|| obj instanceof BigDecimal || obj instanceof Double)
+			return "" + obj;
+
+		// convert date into string
+		if (obj instanceof Date) {
+			if (DateUtil.hasTime((Date) obj)) {
+				return "'"
+						+ DateUtil.dateToString((Date) obj,
+								"yyyy-MM-dd HH:mm:ss") + "'";
+			} else
+				return "'" + DateUtil.dateToString((Date) obj, "yyyy-MM-dd")
+						+ "'";
+		}
+
+//		if (obj instanceof DateTime) {
+//			DateTime dateTime = (DateTime) obj;
+//			return "'" + dateTime.toString("yyyy-MM-dd HH:mm:ss") + "'";
+//		}
+
+		if (obj instanceof Boolean) {
+			return (((Boolean) obj).booleanValue()) ? "1" : "0";
+		}
+
+		if (obj instanceof BaseEntity
+				|| BaseEntity.class.isAssignableFrom(obj.getClass())) {
+			return "" + ((BaseEntity) obj).getId();
+		}
+
+		if (obj.toString().length() > 0) {
+			String queryValue = "" + obj;
+			queryValue = queryValue.replaceAll("'", "\\\\'");
+			return "'" + queryValue + "'";
+		} else {
+			return "";
+		}
+	}
+}
---
+++ opentides3-core/src/test/java/org/opentides/util/SyncUtilTest.java
@@ -0,0 +1,66 @@
+package org.opentides.util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opentides.bean.SystemCodes;
+import org.opentides.util.DateUtil;
+import org.opentides.util.SyncUtil;
+
+public class SyncUtilTest {
+
+	@Test
+	public void buildInsertStatement() {
+		SystemCodes sc = new SystemCodes("category1","key1","value1");
+		SyncUtil.buildInsertStatement(sc);
+		Assert.assertEquals(
+				"insert into system_codes (KEY_,VALUE_,CATEGORY_) values ('KEY1','value1','CATEGORY1')",
+				SyncUtil.buildInsertStatement(sc));
+		
+		Date now = new Date();
+		sc.setCreateDate(now);
+		String nowString = DateUtil.dateToString(now, "yyyy-MM-dd HH:mm:ss");
+		Assert.assertEquals(
+				"insert into system_codes (CREATEDATE,KEY_,VALUE_,CATEGORY_) values ('"
+						+ nowString + "','KEY1','value1','CATEGORY1')",
+						SyncUtil.buildInsertStatement(sc));
+		
+		SystemCodes p = new SystemCodes("categoryP","keyP","parentP");
+		p.setId(2l);
+		sc.setParent(p);
+		sc.setId(3l);
+		Assert.assertEquals(
+				"insert into system_codes (ID,CREATEDATE,KEY_,VALUE_,CATEGORY_,PARENT_) values (3,'"
+						+ nowString + "','KEY1','value1','CATEGORY1',2)",
+						SyncUtil.buildInsertStatement(sc));
+		
+	}
+	
+	@Test
+	public void buildUpdateStatement() {
+		SystemCodes _new = new SystemCodes("category2","key1","value2");
+		_new.setId(1l);
+		
+		List<String> updatedFields = new ArrayList<String>();
+		updatedFields.add("category");
+		updatedFields.add("value");
+
+		Assert.assertEquals(
+				"update system_codes set CATEGORY_='CATEGORY2',VALUE_='value2' where id=1",
+				SyncUtil.buildUpdateStatement(_new, updatedFields));
+		
+	}
+	
+	@Test
+	public void buildDeleteStatement() {
+		SystemCodes sc = new SystemCodes("category1","key1","value1");
+		sc.setId(1l);
+		
+		Assert.assertEquals("delete from system_codes where id=1",
+				SyncUtil.buildDeleteStatement(sc));
+	}
+
+}
